9c742f838fbba487920ccdb252771342d7e6faa4
Chris Weber <christopher.m.weber@gmail.com>
2025-06-10 21:40:09 -0500

    Add end-to-end tests

---
+++ __tests__/e2e.test.js
@@ -0,0 +1,66 @@
+// @jest-environment node
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+const request = require('supertest');
+
+let app;
+
+describe('end-to-end server', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    process.env.YOUTUBE_API_KEY = 'test';
+    app = require('../server');
+  });
+
+  test('session flow add and complete song', async () => {
+    const sessionRes = await request(app).post('/sessions');
+    expect(sessionRes.statusCode).toBe(200);
+    const { id, code } = sessionRes.body;
+    expect(id).toBeDefined();
+    expect(code).toBeDefined();
+
+    const joinRes = await request(app)
+      .post(`/sessions/${code}/join`)
+      .send({ name: 'Alice' });
+    expect(joinRes.statusCode).toBe(200);
+    expect(joinRes.body.sessionId).toBe(id);
+
+    const songRes = await request(app)
+      .post('/songs')
+      .send({ videoId: 'ABCDEFGHIJK', singer: 'Alice' });
+    expect(songRes.statusCode).toBe(200);
+    const songId = songRes.body.id;
+
+    const queueRes = await request(app).get('/queue');
+    expect(queueRes.statusCode).toBe(200);
+    expect(queueRes.body.queue.length).toBe(1);
+    expect(queueRes.body.queue[0].id).toBe(songId);
+
+    const completeRes = await request(app).post(`/songs/${songId}/complete`);
+    expect(completeRes.statusCode).toBe(200);
+
+    const queueEmpty = await request(app).get('/queue');
+    expect(queueEmpty.body.queue.length).toBe(0);
+  });
+
+  test('fair queue ordering', async () => {
+    const sessionRes = await request(app).post('/sessions');
+    const { code } = sessionRes.body;
+
+    await request(app).post(`/sessions/${code}/join`).send({ name: 'A' });
+    await request(app).post(`/sessions/${code}/join`).send({ name: 'B' });
+
+    await request(app).post('/songs').send({ videoId: 'AAAAAAAAAAA', singer: 'A' });
+    await request(app).post('/songs').send({ videoId: 'BBBBBBBBBBB', singer: 'A' });
+    await request(app).post('/songs').send({ videoId: 'CCCCCCCCCCC', singer: 'B' });
+
+    const queueRes = await request(app).get('/queue');
+    expect(queueRes.statusCode).toBe(200);
+    expect(queueRes.body.queue.map((s) => s.videoId)).toEqual([
+      'AAAAAAAAAAA',
+      'CCCCCCCCCCC',
+      'BBBBBBBBBBB',
+    ]);
+  });
+});
